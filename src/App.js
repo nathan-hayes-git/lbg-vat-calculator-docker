b3457d2773eadf34c96bb5efbeec6f13174f93c0
Nathan <Nathan/Hayes@lloydsbanking.com>
2024-11-07 12:09:30 +0000

    EDIT added name to top of calculator

--- src/App.js
+++ src/App.js
@@ -35,14 +35,18 @@ function App() {
 
   return (
     <div className='header field'>
-      VAT CALCULATOR
-      <div className='colour-border'>
-        <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
-        <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
-        <DisplayBlock customstyle="field" label="VAT to pay: " value={vatToPay} />
-        <PriceEntryField customstyle="field" label="Price incl VAT: " priceChanged={handleGrossPriceChange} price={grossPrice === 0.0 ? "" : grossPrice} />
+      Nathan Hayes
+      <div className='header field'>
+        VAT CALCULATOR
+        <div className='colour-border'>
+          <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
+          <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
+          <DisplayBlock customstyle="field" label="VAT to pay: " value={vatToPay} />
+          <PriceEntryField customstyle="field" label="Price incl VAT: " priceChanged={handleGrossPriceChange} price={grossPrice === 0.0 ? "" : grossPrice} />
+        </div>
       </div>
     </div>
+    
   );
 }
 
